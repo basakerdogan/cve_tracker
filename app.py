e4d4a0a0dd9d841c9527528db550921343aafac3
Başak Erdoğan <edabasakerdogan@gmail.com>
2024-04-10 05:10:38 +0300

    adding search feature

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask,request, render_template, url_for,redirect
 import requests,json
 
 app = Flask(__name__)
@@ -17,7 +17,17 @@ def main_page():
 
     return render_template('main_page.html', cve_dict = cve_dict, cve_len = len(cve_dict))
 
+@app.route('/cve/<cve_id>',methods=['POST','GET'])
+def cve(cve_id):
+    cve_id = request.form['cve_id']
+    api_url = f"https://services.nvd.nist.gov/rest/json/cves/2.0?cveId={cve_id}"
+    req = requests.get(url = api_url)
+    data = json.loads(req.text)
+    cve_desc = data["vulnerabilities"][0]["cve"]["descriptions"][0]["value"]
+    cve_base_severity = data["vulnerabilities"][0]["cve"]["metrics"]["cvssMetricV2"][0]["baseSeverity"]
+    return render_template('detail.html', cve_desc = cve_desc , cve_base_severity = cve_base_severity, cve_id= cve_id)
+
 
 if __name__ == '__main__':
+    app.run(debug=True)
 
-	app.run()
